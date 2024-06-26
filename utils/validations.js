74cbf0580617c79efe13bea9cdb37b4a733dcef2
lilsecret <chuckm248@gmail.com>
2024-06-26 15:15:28 -0600

    make a function to see if two arrays are equal

--- utils/validations.js
+++ utils/validations.js
@@ -5,3 +5,17 @@ const validatePokemonName = (name) => {
   );
   return pokemonArr.length > 0 ? pokemonArr : false;
 };
+
+function arraysAreEqual(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
